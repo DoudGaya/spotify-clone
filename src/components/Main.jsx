bab8079723efa4ce12fa832efbdaee85f7b999dd
DoudGaya <arbdoule@yahoo.com>
2022-12-25 18:52:12 +0100

    changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,7 @@
+// IMPORTS 
 import {React, useState} from "react"
 import banners from "../music/banners"
 import Banner from "./Musics/Banner"
-
-
 
 export default function Main() {
 
@@ -10,38 +9,39 @@ export default function Main() {
     // STATES DECLARATIONS     
     const [banner, setBanner] = useState(banners)
     const [music, setMusic] = useState(banners)
-    const [bannerBG, setBannerBG] = useState('yellow')
+    const [bannerBG, setBannerBG] = useState('')
 
    
     // FUNCTIONS 
     function bannerV(p) {
        setBannerBG( prev => {
+
         return prev = p
+        
        })
-       console.log(bannerBG)
+
     }
 
     const song = banner.map( item => {
         return (<Banner key={item.id} func={bannerV} song={item}/>)
     })
-
-
-
         const single = music.map( (sing) => {
+            
             return (
                 <>
-                    <div className="flex flex-col backdrop-blur-md bg-stone-700/30">
-                        <div className=" flex w-full p-4 ralative">
-                            <img src={sing.banner} alt="" className="drop-shadow-xl border-b-4 rounded-md srcset=" />
-                            <div className="absolute bottom-10 px-2 border-l-4">
-                                <p className=" font-spotify">2010 Mix</p>
-                            </div>
+                <div className="flex flex-col backdrop-blur-md bg-stone-700/30">
+                    <div className=" flex w-full p-4 ralative">
+                        <img src={sing.banner} alt="" className="drop-shadow-xl border-b-4 rounded-md srcset=" />
+                        <div className="absolute bottom-10 px-2 border-l-4">
+                            <p className=" font-spotify">2010 Mix</p>
                         </div>
                     </div>
+                </div>
                 </>
             )
         })
 
+        
     return (
         <div className=" bg-stone-900 flex-1 rounded-lg flex flex-col">
                 <div className={`p-6 rounded-t-lg bg-gradient-to-b transition-all ease-linear delay-100 duration-150 from-${bannerBG}-700/50 via-${bannerBG}-900/50 to-stone-900/50 flex flex-col`}>
--- src/songs.js
+++ src/songs.js
@@ -6,5 +6,60 @@ export default [
         Artist: 'Alan Walker',
         color: '#333333',
         banner: ''
+    },
+
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
+    },
+
+    
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
+    },
+
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
+    },
+
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
+    },
+
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
+    },
+
+    {
+        mix: 'Happy Mix' ,
+        title: 'Jay Z, Brick and Lace, Rihanna and more',
+        genre: 'Blues',
+        Artist: 'Alan Walker',
+        color: '#333333',
+        banner: ''
     }
 ]
